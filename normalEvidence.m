679fc0eaf3f6dd748ad257e6ea0a7e121823d408
SirMew <drenilef@gmail.com>
2024-10-11 19:27:10 +1300

    test push

--- normalEvidence.m
+++
@@ -1,4 +0,0 @@
-% get the normal (guassian) evidence to create a likelihood model
-function evidence = normalEvidence(data, mu, sigma)
-
-evidence = 10*((log(1/sigma/sqrt(2*pi)) -0.5*((data-mu)/sigma).^2 )/log(10));
--- normalEvidence.py
+++
@@ -1,8 +0,0 @@
-# get the normal (guassian) evidence to create a likelihood model
-import numpy as np
-
-def normalEvidence(data, mu, sigma):
-
-evidence = 10*((np.log(1/sigma/sqrt(2*np.pi)) -0.5*((data-mu)/sigma).^2 )/np.log(10));
-
-return evidence
